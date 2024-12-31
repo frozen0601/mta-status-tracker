e58d711b622316d9d50fa660b2c0f780f359c51f
Andy Huang <71y716andy@gmail.com>
2024-12-31 15:16:57 -0500

    refine docker init

--- src/backend/apps/subway/apps.py
+++ src/backend/apps/subway/apps.py
@@ -7,15 +7,4 @@ logger = logging.getLogger(__name__)
 
 
 class SubwayConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "subway"
-
-    def ready(self):
-        from .models import SubwayLine
-
-        # Define all MTA subway lines
-        from .mta_data_fetcher import mta_client
-
-        subway_lines = mta_client.get_subway_line_names()
-        for line in subway_lines:
-            SubwayLine.objects.get_or_create(name=line)
